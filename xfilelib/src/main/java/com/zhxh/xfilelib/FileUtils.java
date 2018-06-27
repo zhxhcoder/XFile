ceab83efdf77f2958d46e4ef71c686511c0adb8f
zhxh <zhxhcoder@gmail.com>
2018-06-27 10:34:36 +0800

    FileUtils

---
+++ xfilelib/src/main/java/com/zhxh/xfilelib/FileUtils.java
@@ -0,0 +1,39 @@
+package com.zhxh.xfilelib;
+
+import java.io.File;
+
+public class FileUtils {
+
+    private static void searchFile(String path,String filename) {
+
+        File fileDirectory = new File(path);
+
+        File[] fileList = fileDirectory.listFiles();
+
+        for (int j = 0; j < fileList.length; j++) {
+
+            File file = fileList[j];
+
+            String fileName = file.getName();
+
+            if (fileName.contains(".apk")) {
+
+                String tempFileName = fileName.substring(0, fileName.indexOf(".apk"));
+
+                if (tempFileName.contains("-release")) {
+
+                    String newFileName = filename + tempFileName.substring(tempFileName.indexOf("-") + 1, tempFileName.indexOf("-release"));
+
+                    System.out.println(newFileName);
+
+                    file.renameTo(new File(fileDirectory + "/" + newFileName + ".apk"));
+                } else {
+                    System.out.println("已经改名");
+                }
+
+            }
+        }
+
+    }
+
+}
