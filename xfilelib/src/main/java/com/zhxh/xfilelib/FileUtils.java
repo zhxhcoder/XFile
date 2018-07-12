70af5bfc1bd26d4dc53b5b72d031bb2fd5400082
zhxh <zhxhcoder@gmail.com>
2018-07-12 12:55:30 +0800

    getMatchFiles

--- xfilelib/src/main/java/com/zhxh/xfilelib/FileUtils.java
+++ xfilelib/src/main/java/com/zhxh/xfilelib/FileUtils.java
@@ -13,32 +13,21 @@ public class FileUtils {
     }
 
     public static List<String> getMatchFiles(String dirPath, String regRex) {
-        List<String> resultList = new ArrayList<>();
-        File fileDirectory = new File(dirPath);
-        File[] fileList = fileDirectory.listFiles();
-        if (fileList == null) {
-            return resultList;
-        }
-        for (File file : fileList) {
-            if (!file.isDirectory()) {
-                String fileName = file.getName();
-                if (null == regRex || regRex.length() == 0) {
-                    resultList.add(fileName);
-                } else {
-                    Pattern pattern = Pattern.compile(regRex);
-                    Matcher matcher = pattern.matcher(fileName);
-                    if (matcher.find()) {
-                        resultList.add(fileName);
-                    }
-                }
-            }
-        }
-        return resultList;
+        return getMatchFiles(null, false, dirPath, regRex);
     }
 
-    public static List<String> getMatchFiles(List<String> resultList,boolean isNest, String dirPath, String regRex) {
-        if (resultList==null) {
-            resultList=new ArrayList<>();
+    /**
+     * 输出顺序是从树最深层遍历
+     *
+     * @param resultList 输出位置
+     * @param isNest     是否嵌套
+     * @param dirPath    目录位置
+     * @param regRex     匹配的正则
+     * @return 能匹配的文件名
+     */
+    public static List<String> getMatchFiles(List<String> resultList, boolean isNest, String dirPath, String regRex) {
+        if (resultList == null) {
+            resultList = new ArrayList<>();
         }
         File fileDirectory = new File(dirPath);
         File[] fileList = fileDirectory.listFiles();
@@ -59,7 +48,7 @@ public class FileUtils {
                 }
             } else {
                 if (isNest) {
-                    getMatchFiles(resultList,true, file.getAbsolutePath(), regRex);
+                    getMatchFiles(resultList, true, file.getAbsolutePath(), regRex);
                 }
             }
         }
